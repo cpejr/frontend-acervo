751b15158c9582be574877db92e893add7da5fdc
SamuelFVG <samuelfelipeif@gmail.com>
2024-02-26 11:24:26 -0300

    feat: test code

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as Header } from "./features/layout/Header/Header.jsx";
 export { default as Footer } from "./features/layout/Footer/Footer.jsx";
-export { default as AppLayout } from "./features/layout/AppLayout/AppLayout.jsx";
