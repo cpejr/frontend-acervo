65b1cf139ed4ab0800d06f96cd34448201feaddf
Joaovitorleao <97983498+Joaovitorleao@users.noreply.github.com>
2024-03-04 20:16:29 -0300

    feat: add google codes for firebase

---
+++ src/Stores/auth.js
@@ -0,0 +1,15 @@
+import jwtDecode from "jwt-decode";
+import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  auth: null,
+  setAuth: (accessToken) => {
+    const { user } = jwtDecode(accessToken);
+
+    set({ auth: { accessToken, user } });
+  },
+  setUser: (user) => set((state) => ({ auth: { ...state?.auth, user } })),
+  clearAuth: () => set({ auth: null }),
+}));
+
+export default useAuthStore;
---
+++ src/services/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { firebaseConfig } from "./firebaseConfig";
+
+const provider = new firebase.GoogleAuthProvider();
+
+// Initialize Firebase
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.getAuth(app);
+
+// Whenever a user interacts with the provider, we force them to select an account
+provider.setCustomParameters({
+  prompt: "select_account ",
+});
+
+// Apply the default browser language preference
+
+auth.useDeviceLanguage();
+
+export const signInWithGooglePopup = () =>
+  firebase.signInWithPopup(auth, provider);
+
+//for mobile it is better to use redirect
+export const signInWithGoogleRedirect = () =>
+  firebase.signInWithRedirect(auth, provider);
+export const getGoogleRedirectResult = () => firebase.getRedirectResult(auth);
