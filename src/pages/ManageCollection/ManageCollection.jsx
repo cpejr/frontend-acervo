579935c12acbb32bc35037559e18e09b414a7bf3
ZeL-ucas <zelucaspomps15@gmail.com>
2024-03-06 08:51:39 -0300

    feat:inicial settings , table test

--- src/pages/ManageCollection/ManageCollection.jsx
+++ src/pages/ManageCollection/ManageCollection.jsx
@@ -1,3 +1,32 @@
+import { useState, useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { Container, Title } from "./Styles";
+import { Table } from "../../components";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { AiOutlineCloseCircle, AiFillTool } from "react-icons/ai";
 export default function ManageCollection() {
-  return <div>ManageCollection</div>;
+  const columns = [
+    { field: "name", header: "Name" },
+    { field: "Description", header: "Description" },
+    { field: "manage", header: "Manage" },
+  ];
+  let data = [
+    {
+      name: "jose",
+      Description: "ola",
+      manage: (
+        <div>
+          <AiFillTool />
+          <RiDeleteBin5Line />
+        </div>
+      ),
+    },
+  ];
+  return (
+    <Container>
+      <Title>Gerenciar Arquivos </Title>
+      <Table columns={columns} data={data}></Table>
+    </Container>
+  );
 }
