0115a57b966b446bdf87274ab264098847febda2
Joaovitorleao <97983498+Joaovitorleao@users.noreply.github.com>
2024-03-05 22:16:25 -0300

    feat: functions to make login with google
    user.js, endpoints.js, api.js and functions in header.jsx

--- src/components/features/layout/Header/Header.jsx
+++ src/components/features/layout/Header/Header.jsx
@@ -1,8 +1,28 @@
+import useAuthStore from "../../../../Stores/auth";
 import { Link } from "../../../../components";
+import { login } from "../../../../services/endpoints";
 
 import { Container } from "./Styles";
 
 export default function Header() {
+
+  const { auth, setAuth } = useAuthStore.getState();
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+
+  const logGoogleUser = async () => {
+    if(auth.accessToken() === null) {
+      const googleResponse = await signInWithGooglePopup();
+      const response = login({
+        name: googleResponse?.user?.displayName,
+        email:  googleResponse?.user?.email,
+        imageURL: googleResponse?.user?.photoURL,
+      });
+        setAuth(response.token);
+    } else {
+      clearAuth();
+    }
+  };
+
   return (
     <Container>
       <Link to={"/story"}>E-commerce</Link>
@@ -10,7 +30,7 @@ export default function Header() {
       <Link>Eventos Culturais</Link>
       <Link>Área escolar</Link>
       <Link>Sobre o projeto</Link>
-      <Link>PERFIL</Link>
+      <Link onClick={() => logGoogleUser()}>PERFIL</Link>
     </Container>
   );
 }
---
+++ src/services/endpoints.js
@@ -0,0 +1,27 @@
+import useAuthStore from "../Stores/auth";
+import api from "./api";
+//user
+
+export async function getUsers() {
+  const { data } = await api.get("/user");
+  return data;
+}
+
+export async function deleteUser(_id) {
+  const { data } = await api.delete(`/user/${_id}`);
+
+  return data;
+}
+
+export async function updateUser({ _id, newUserData }) {
+  const { data } = await api.put(`/user/${_id}`, newUserData);
+
+  return data;
+}
+export const login = async (credentials) => {
+    const { setAuth } = useAuthStore.getState();
+    const { data } = await api.post('/login', credentials);
+
+    setAuth(data.accessToken);
+    return data;
+}
