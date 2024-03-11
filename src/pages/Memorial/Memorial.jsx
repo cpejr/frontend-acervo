015560bb29e83c0bfec9abd8790be4eb54f51ec8
Mateus Bretz <mateusbretz@cpejr.com.br>
2024-03-11 16:14:24 -0300

    feat: add filters

--- src/pages/Memorial/Memorial.jsx
+++ src/pages/Memorial/Memorial.jsx
@@ -1,11 +1,59 @@
-import { Container, Title } from "../Memorial/Styles";
+import {
+  Container,
+  Title,
+  Filter,
+  Characteristics,
+  DivSelect,
+  FilterTitle,
+  UniSelect,
+  StyledCheckbox,
+  VerticalLine,
+} from "../Memorial/Styles";
 import { SearchBar } from "../../components";
 
+// const cardData = [
+//   { _id: 1, title: "Card 1", description: "Descrição do Card 1" },
+//   { _id: 2, title: "Card 2", description: "Descrição do Card 2" },
+//   { _id: 3, title: "Card 3", description: "Descrição do Card 3" },
+//   { _id: 4, title: "Card 4", description: "Descrição do Card 4" },
+//   { _id: 5, title: "Card 5", description: "Descrição do Card 5" },
+// ];
+
 export default function Memorial() {
+  const filters = [
+    { label: "Melhor avaliados", value: "melhorAvaliados" },
+    { label: "Favoritos", value: "favoritos" },
+  ];
+
+  const characteristicCheckboxes = [
+    { label: "Característica 1" },
+    { label: "Característica 2" },
+    { label: "Característica 3" },
+  ];
+
   return (
     <Container>
       <Title>ACERVO</Title>
       <SearchBar></SearchBar>
+      <Filter>
+        <Characteristics>
+          <FilterTitle>Características:</FilterTitle>
+          {characteristicCheckboxes.map((checkbox, index) => (
+            <label key={index}>
+              <StyledCheckbox />
+              {checkbox.label}
+            </label>
+          ))}
+        </Characteristics>
+        <VerticalLine />
+        <DivSelect>
+          <UniSelect
+            options={filters}
+            optionLabel="label"
+            placeholder="Ordenar Por"
+          />
+        </DivSelect>
+      </Filter>
     </Container>
   );
 }
--- src/pages/Memorial/Styles.js
+++ src/pages/Memorial/Styles.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { breakpoints, colors } from "../../styles/stylesVariables";
+import { Dropdown } from "primereact/dropdown";
+import { Checkbox } from "primereact/checkbox";
 
 export const Container = styled.div`
   display: flex;
@@ -23,5 +25,97 @@ export const Title = styled.div`
   }
   @media (max-width: ${breakpoints.smallDevice}) {
     font-size: 22px;
+  }
+`;
+
+export const DivLine = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+`;
+
+export const Line = styled.div`
+  display: flex;
+  width: 90%;
+  height: auto;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 2rem;
+  flex-wrap: wrap;
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const Characteristics = styled.div`
+  display: flex;
+  flex-direction: column;
+  > label {
+    align-items: center;
+    display: flex;
+    gap: 0.5rem;
+    padding-top: 0.3rem;
+    color: #eedac5;
+  }
+`;
+
+export const VerticalLine = styled.div`
+  border-left: 2px solid white;
+  height: 10rem;
+`;
+
+export const FilterTitle = styled.div`
+  display: flex;
+  color: ${colors.accent};
+  padding-bottom: 1rem;
+`;
+
+export const StyledCheckbox = styled(Checkbox)`
+  width: 20px;
+  height: 20px;
+  border: 2px solid ${colors.darkwhite};
+  border-radius: 5px;
+  justify-content: center;
+`;
+
+export const DivSelect = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  border: 2px solid white;
+  border-radius: 5px;
+  width: 10%;
+  @media (max-width: ${breakpoints.miniTablet}) {
+    width: 40%;
+  }
+  @media (max-width: ${breakpoints.miniTablet}) {
+    flex-direction: column;
+    width: 20%;
+  }
+`;
+
+export const UniSelect = styled(Dropdown)`
+  gap: 1rem;
+  .p-dropdown-label {
+    color: white;
+    width: 100%;
+  }
+  .p-inputtext {
+    color: white;
+  }
+  @media (max-width: ${breakpoints.miniTablet}) {
+    width: 30%;
+  }
+  @media (max-width: ${breakpoints.smallMobile}) {
+    width: 20%;
   }
 `;
