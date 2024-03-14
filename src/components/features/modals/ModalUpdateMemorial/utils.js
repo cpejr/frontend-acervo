a545e596f89a2648f588d3500b790fe37eedfbba
ZeL-ucas <zelucaspomps15@gmail.com>
2024-03-14 07:58:31 -0300

    fix:update shchema working properly

--- src/components/features/modals/ModalUpdateMemorial/utils.js
+++ src/components/features/modals/ModalUpdateMemorial/utils.js
@@ -1,17 +1,20 @@
 import { z } from "zod";
 
-export const updateCollectionValidationSchema = z.object({
-  title: z
-    .string()
-
-    .optional(),
-  link: z
-    .string()
-
-    .optional(),
-
-  archive: z
-    .string()
-
-    .optional(),
-});
+export const updateCollectionValidationSchema = z
+  .object({
+    title: z.string().optional(),
+    archive: z.string().optional(),
+    link: z.string().optional(),
+  })
+  .partial()
+  .refine(
+    (obj) => {
+      for (const val of Object.values(obj)) {
+        if (val !== undefined && val !== "") return true;
+      }
+      return false;
+    },
+    {
+      message: "Pelo menos um dos campos deve ser preenchido ",
+    }
+  );
