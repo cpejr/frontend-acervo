ba577f196cb1c1b8b9074baddfed701f12c354ff
SamuelFVG <samuelfelipeif@gmail.com>
2024-03-19 08:55:59 -0300

    feat: add initial header code and solve conflicts

--- src/components/features/layout/Header/Header.jsx
+++ src/components/features/layout/Header/Header.jsx
@@ -1,16 +1,50 @@
 import { Link } from "../../../../components";
-
-import { Container } from "./Styles";
+import { Container, City, Column, SocialMedias, Button } from "./Styles";
+import {
+  Login,
+  LogoCidade,
+  Whatsapp,
+  Facebook,
+  Instagram,
+} from "../../../../assets/index";
+import { HamburgerMenu } from "../../../index";
+import React from "react";
 
 export default function Header() {
+  const usuario = true;
+
   return (
     <Container>
-      <Link to={"/story"}>E-commerce</Link>
+      <City src={LogoCidade}></City>
+      <Link to={"/story"}>História</Link>
       <Link>Acervo</Link>
-      <Link>Eventos Culturais</Link>
-      <Link>Área escolar</Link>
-      <Link>Sobre o projeto</Link>
-      <Link>PERFIL</Link>
+      <Link to={"/AboutUs"}>Sobre</Link>
+      <Link to={"/events"}>Eventos</Link>
+      <Link to={"/Support"}> Apoiar</Link>
+      {usuario ? (
+        <React.Fragment>
+          <Link to={"/manageusers"}>Usuários</Link>
+          <Link to={"/managecollection"}>Memorial</Link>
+          <Link to={"/managevents"}>Eventos</Link>
+        </React.Fragment>
+      ) : null}
+      <Column>
+        <Button>
+          Entrar<img src={Login} width="20px"></img>
+        </Button>
+        <SocialMedias>
+          <a href="https://www.instagram.com/prefeiturabd/">
+            <img src={Instagram} alt="Logo Instagram" width="75%"></img>
+          </a>
+          <a href="https://www.facebook.com/prefeiturabd/?locale=pt_BR">
+            <img src={Facebook} alt="Logo Facebook" width="75%"></img>
+          </a>{" "}
+          <a href="http://wa.me/+31992740294">
+            <img src={Whatsapp} alt="Logo Whatsapp" width="75%"></img>
+          </a>
+        </SocialMedias>
+      </Column>
+      <HamburgerMenu />
     </Container>
   );
 }
--- src/components/features/layout/Header/Styles.js
+++ src/components/features/layout/Header/Styles.js
@@ -1,11 +1,72 @@
 import styled from "styled-components";
+import { breakpoints } from "../../../../styles/stylesVariables";
+import { Menu } from "antd";
 
 export const Container = styled.div`
-  height: 5rem;
-  width: 100%;
-
+  height: 7rem;
+  max-width: 100%;
   display: flex;
   background-color: black;
   align-items: center;
   justify-content: space-evenly;
+`;
+
+export const City = styled.img`
+  width: 50px;
+  display: flex;
+  margin-left: 3rem;
+  @media (max-width: ${breakpoints.smallDevice}) {
+    margin-left: 0;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding-left: 15px;
+  padding-right: 0;
+  border-left: 2px solid white;
+  @media (max-width: ${breakpoints.smallTablet}) {
+    border-left: 0;
+  }
+`;
+
+export const SocialMedias = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  @media (max-width: ${breakpoints.smallTablet}) {
+    display: none;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: black;
+  color: orange;
+  padding: 7px;
+  border: 1px solid orange;
+  border-radius: 5px;
+  width: 60%;
+  justify-content: space-evenly;
+  margin-left: 20%;
+  display: flex;
+  align-items: center;
+  padding-left: 0;
+  @media (max-width: ${breakpoints.smallTablet}) {
+    width: 110%;
+    margin-left: 0;
+  }
+`;
+
+export const Hamburguer = styled(Menu)`
+  width: 0rem;
+  border-bottom: none;
+  background-color: black;
+  @media (max-width: ${breakpoints.smallTablet}) {
+    width: 6rem;
+  }
+  @media (max-width: ${breakpoints.smallDevice}) {
+    width: 4rem;
+  }
 `;
