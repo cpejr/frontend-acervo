4c53f1c75d0b7f4206d99776872e943f5e2d8522
Rian-Rero <rian@dharma.com.br>
2024-04-01 15:14:07 -0300

    fix: authstore

--- src/Stores/auth.js
+++ src/Stores/auth.js
@@ -1,5 +1,5 @@
 import { jwtDecode } from "jwt-decode";
-import create from "zustand";
+import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 const useAuthStore = create(
@@ -18,7 +18,6 @@ const useAuthStore = create(
     }),
     {
       name: "AuthData",
-      getStorage: () => localStorage,
     }
   )
 );
