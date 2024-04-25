36dbc2619da80301c493e6608b0f99bef85ebcbc
SamuelFVG <samuelfelipeif@gmail.com>
2024-04-25 08:56:35 -0300

    fix: rename styles.js

--- src/components/common/FormInput/FormInput.jsx
+++ src/components/common/FormInput/FormInput.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Container, IconContainer, StyledInput } from "./styles";
+import { Container, IconContainer, StyledInput } from "./Styles";
 
 export default function FormInput({
   inputKey,
