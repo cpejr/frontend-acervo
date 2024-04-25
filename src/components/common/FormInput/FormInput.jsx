1d6be4a53b6c236c194223af07dd4067ff1fcac9
SamuelFVG <samuelfelipeif@gmail.com>
2024-04-25 08:54:44 -0300

    fix: fix import error

--- src/components/common/FormInput/FormInput.jsx
+++ src/components/common/FormInput/FormInput.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Container, IconContainer, StyledInput } from "./Styles";
+import { Container, IconContainer, StyledInput } from "./styles";
 
 export default function FormInput({
   inputKey,
