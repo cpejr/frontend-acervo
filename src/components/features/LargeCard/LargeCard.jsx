db4c7c4c26244898db5c025a32252172d030e8a2
mateus <mateusbretz@cpejr.com.br>
2024-05-06 23:01:43 -0300

    fix: rendering images

--- src/components/features/LargeCard/LargeCard.jsx
+++ src/components/features/LargeCard/LargeCard.jsx
@@ -11,33 +11,30 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useGetArchives } from "../../../hooks/querys/archive";
 
-// const images = [
-//   "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/54/b8/ac/noturna.jpg?w=500&h=500&s=1",
-//   "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/54/b8/ac/noturna.jpg?w=500&h=500&s=1",
-//   "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/54/b8/ac/noturna.jpg?w=500&h=500&s=1",
-// ];
-
 export default function LargeCard({ data }) {
   const { title, description, shortDescription, archive } = data;
   const archiveIDs = archive.map((file) => file._id);
   const formatedArchives = archiveIDs.join(", ");
-  console.log(formatedArchives);
-
-  const {
-    data: archiveData,
-    isLoading,
-    isError,
-  } = useGetArchives(archiveIDs, "archives");
+  const { data: archiveData, isLoading } = useGetArchives(
+    formatedArchives,
+    title,
+    {
+      onError: (err) => {
+        console.error("Erro ao pegar itens", err);
+      },
+    }
+  );
+  console.log(archiveData);
 
   return (
     <StyledCard>
       {isLoading && <p>Carregando...</p>}
-      {isError && <p>Ocorreu um erro ao carregar os arquivos.</p>}
+      {/* {isError && <p>Ocorreu um erro ao carregar os arquivos.</p>} */}
       {archiveData && (
         <Carousel showStatus={false} showIndicators={false} showThumbs={false}>
           {archiveData.map((file, index) => (
             <div key={index}>
-              <img alt={file.name} src={file.url} />
+              <img src={file} />
             </div>
           ))}
         </Carousel>
