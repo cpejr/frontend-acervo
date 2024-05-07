7b957583322703e446c70d04e08e60dbe4ceb42c
mateus <mateusbretz@cpejr.com.br>
2024-05-07 12:13:13 -0300

    fix: adding conditionals for each file type

--- src/components/features/LargeCard/LargeCard.jsx
+++ src/components/features/LargeCard/LargeCard.jsx
@@ -29,12 +29,36 @@ export default function LargeCard({ data }) {
   return (
     <StyledCard>
       {isLoading && <p>Carregando...</p>}
-      {/* {isError && <p>Ocorreu um erro ao carregar os arquivos.</p>} */}
       {archiveData && (
         <Carousel showStatus={false} showIndicators={false} showThumbs={false}>
           {archiveData.map((file, index) => (
             <div key={index}>
-              <img src={file} />
+              {file.startsWith("data:image") && (
+                <img src={file} alt={`Imagem ${index}`} />
+              )}
+              {file.startsWith("data:video") && (
+                <video controls width="100%" height="auto">
+                  <source src={file} type="video/mp4" />
+                  Seu navegador não suporta o elemento de vídeo.
+                </video>
+              )}
+              {file.startsWith("data:audio") && (
+                <audio controls>
+                  <source src={file} type="audio/mpeg" />
+                  Seu navegador não suporta o elemento de áudio.
+                </audio>
+              )}
+              {file.startsWith("data:application/pdf") && (
+                <object
+                  data={file}
+                  type="application/pdf"
+                  width="100%"
+                  height="400px"
+                >
+                  Seu navegador não suporta visualização de PDF. Você pode{" "}
+                  <a href={file}>baixá-lo aqui</a>.
+                </object>
+              )}
             </div>
           ))}
         </Carousel>
