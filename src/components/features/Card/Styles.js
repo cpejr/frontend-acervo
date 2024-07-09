764e2b15f24d2b6d3607c9722fa19876c495ab5a
José Lucas Lacerda Campos <JoseCampos@MacBook-Air-de-Consultoria.local>
2024-07-09 15:49:22 -0300

    fix:card tag size

--- src/components/features/Card/Styles.js
+++ src/components/features/Card/Styles.js
@@ -163,7 +163,7 @@ export const Tags = styled.div`
 `;
 export const Tag = styled.div`
   display: flex;
-  width: 7rem;
+  width: 5.5rem;
   padding: 5px 0.8rem;
   height: 1.5rem;
   border: 1px solid black;
