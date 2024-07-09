65e05ab8f4dbcf06af5ca79b20bd7bbf147f1b29
ZeL-ucas <zelucaspomps15@gmail.com>
2024-07-09 16:04:42 -0300

    fix:trying to put the commit on the air

--- src/components/features/Card/Styles.js
+++ src/components/features/Card/Styles.js
@@ -155,7 +155,7 @@ export const ButtonDiv = styled.div`
 export const Tags = styled.div`
   display: flex;
   flex-direction: row;
-  width: 90%;
+  width: 100%;
   justify-content: first baseline;
   height: auto;
   gap: 1rem;
@@ -163,7 +163,7 @@ export const Tags = styled.div`
 `;
 export const Tag = styled.div`
   display: flex;
-  width: 7rem;
+  width: 3rem;
   padding: 5px 0.8rem;
   height: 1.5rem;
   border: 1px solid black;
